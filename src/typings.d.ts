1d77afc43359a890b7288c296a3a49fd5bdd5019
Cameron Yick <cameron.yick@datadoghq.com>
2021-06-14 21:06:05 -0400

    Add MVP Storybook plugin for Datadog RUM

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,1 +1,10 @@
-declare module "global";
+declare module "global" {
+  export const window: {
+    STORYBOOK_DATADOG_APPLICATION_ID: string;
+    STORYBOOK_DATADOG_CLIENT_TOKEN: string;
+    STORYBOOK_DATADOG_SITE: string;
+    STORYBOOK_DATADOG_SERVICE?: string;
+    STORYBOOK_DATADOG_SAMPLE_RATE?: number;
+    STORYBOOK_DATADOG_TRACK_INTERACTIONS?: boolean;
+  }
+};
